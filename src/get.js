b015d6dac3e82eff3077c693bc97b0c3d6898c56
Amit Goren <amitgoren12@gmail.com>
2020-03-28 23:10:20 +0300

    Cleaned up the code

--- src/get.js
+++ src/get.js
@@ -1,46 +1,41 @@
 const Koa = require("koa");
 const bcrypt = require("bcrypt");
 const aes256 = require("aes256");
-const mysql = require("mysql");
+const mysql = require("promise-mysql");
 
 const config = require("../config.json");
 
 const app = new Koa();
 
 // Connect to the database
-var connection = mysql.createConnection(config.db);
-connection.connect();
+var connection;
+mysql.createConnection(config.db).then((c) => { connection = c; });
 
 app.use(async (ctx) => {
   try {
-    if (ctx.state.user.data.roles.roles.includes("get")) {
-      if (!ctx.request.body.title) {
+    if (!ctx.state.user.data.roles.roles.includes("get")) {
+      ctx.status = 403;
+      return
+    }
+
+    if (!ctx.request.body.title) {
         ctx.status = 400;
         return;
-      }
-      
-      await new Promise((resolve, reject) => {
-        bcrypt.hash(ctx.request.body.title, config.salt, (err, hash) => {
-          const q = "SELECT body FROM entries WHERE title = ?";
-          connection.query(q, hash, (error, results, fields) => {
-            if (error) {
-              ctx.status = 500;
-            } else {
-              if (!results[0].body) {
-                ctx.body = { body: "" };
-              } else {
-                ctx.body = { body: aes256.decrypt(ctx.request.body.title, results[0].body.toString()) };
-              }
-
-              ctx.status = 200;
-              resolve();
-            }
-          });
-        });
-      });
-    } else {
-      ctx.status = 403;
     }
+
+    var hash = await bcrypt.hash(ctx.request.body.title, config.salt);
+      
+    const q = "SELECT body FROM entries WHERE title = ?";
+    const results = await connection.query(q, hash);
+
+    if (!results[0].body) {
+      ctx.body = { body: "" };
+    } else {
+      ctx.body = { body: aes256.decrypt(ctx.request.body.title, results[0].body.toString()) };
+    }
+
+    ctx.status = 200;
+    return;
   } catch (e) {
     ctx.status = 500;
   }
--- src/set.js
+++ src/set.js
@@ -1,42 +1,37 @@
 const Koa = require("koa");
 const bcrypt = require("bcrypt");
 const aes256 = require("aes256");
-const mysql = require("mysql");
+const mysql = require("promise-mysql");
 
 const config = require("../config.json");
 
 const app = new Koa();
 
 // Connect to the database
-var connection = mysql.createConnection(config.db);
-connection.connect();
+var connection;
+mysql.createConnection(config.db).then((c) => { connection = c; });
 
 app.use(async (ctx) => {
   try {
-    if (ctx.state.user.data.roles.roles.includes("set")) {
-      if (!ctx.request.body.title || !ctx.request.body.body) {
-        ctx.status = 400;
-        return;
-      }
-      
-      await new Promise((resolve, reject) => {
-        bcrypt.hash(ctx.request.body.title, config.salt, (err, hash) => {
-          const body = aes256.encrypt(ctx.request.body.title, ctx.request.body.body);
-
-          const q = "INSERT INTO entries SET ? ON DUPLICATE KEY UPDATE body = ?;";
-          connection.query(q, [{ title: hash, body: body }, body], (error, results, fields) => {
-            if (error) {
-              reject();
-            } else {
-              ctx.status = 200;
-              resolve();
-            }
-          });
-        });
-      });
-    } else {
+    if (!ctx.state.user.data.roles.roles.includes("set")) {
       ctx.status = 403;
+      return;
     }
+
+    if (!ctx.request.body.title || !ctx.request.body.body) {
+      ctx.status = 400;
+      return;
+    }
+
+    var hash = await bcrypt.hash(ctx.request.body.title, config.salt);
+    const body = aes256.encrypt(ctx.request.body.title, ctx.request.body.body);
+    
+
+    const q = "INSERT INTO entries SET ? ON DUPLICATE KEY UPDATE body = ?;";
+    const results = await connection.query(q, [{ title: hash, body: body }, body]);
+
+    ctx.status = 200;
+    return;
   } catch (e) {
     ctx.status = 500;
   }
